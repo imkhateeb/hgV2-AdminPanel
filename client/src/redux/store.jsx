96ec10ac8b494f9a0a6c29f79d1770c2b98ece3c
siva222003 <sivapidugu02@gmail.com>
2023-11-18 09:50:58 +0530

    some changes

--- client/src/redux/store.jsx
+++ client/src/redux/store.jsx
@@ -1,12 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore} from '@reduxjs/toolkit';
 import announcementSlice from './slices/announcementSlice';
 import feedSlice from './slices/feedSlice';
-
 const store = configureStore({
   reducer: {
     announcements : announcementSlice,
     feeds : feedSlice,
-  }
+  },
 })
 
 export default store;
